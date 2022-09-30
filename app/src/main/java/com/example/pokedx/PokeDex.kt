34eb78c739c61ab2f0ea00c3c6137df0698c4e29
KeltonFlauber <keltonflauber12@gmail.com>
2022-09-30 10:50:53 -0300

    initial commit

---
+++ app/src/main/java/com/example/pokedx/PokeDex.kt
@@ -0,0 +1,11 @@
+package com.example.pokedx
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class PokeDex : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_poke_dex)
+    }
+}
